f24ab82a6866a82070664baa4f82a1a4bc86c3e4
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2021-03-24 11:56:12 +0100

    initalize project

---
+++ src/main/java/se/Grupp2/Quizmaster/QuizmasterApplication.java
@@ -0,0 +1,13 @@
+package se.Grupp2.Quizmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/Grupp2/Quizmaster/QuizmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package se.Grupp2.Quizmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
