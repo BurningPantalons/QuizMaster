33e2c076d699b044ac9cd8fbcfcd64dcab614c59
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-03-31 11:43:21 +0200

    create addPlayer method

--- src/main/java/se/Grupp2/Quizmaster/services/PlayerService.java
+++ src/main/java/se/Grupp2/Quizmaster/services/PlayerService.java
@@ -3,15 +3,23 @@ package se.Grupp2.Quizmaster.services;
 import org.springframework.stereotype.Service;
 import se.Grupp2.Quizmaster.models.Player;
 
+import java.util.List;
+
 @Service
 public class PlayerService {
-
-    Player[] players;
+    
+    List<Player> players;
 
     public void movePlayer(int playerNumber, int diceRoll, int currentPosition) {
 
         int newPosition = diceRoll + currentPosition;
-        players[playerNumber].setPosition(newPosition);
+        players.get(playerNumber).setPosition(newPosition);
+
+    }
+
+    public void addPlayer(Player player) {
+
+        players.add(player);
 
     }
 }
