2a5754093e0c2942e3cab92c076e134cb21cb884
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-04-07 11:29:18 +0200

    codeAlong with Rasmus & David. Add board model

--- src/main/java/se/Grupp2/Quizmaster/models/Board.java
+++ src/main/java/se/Grupp2/Quizmaster/models/Board.java
@@ -1,12 +1,21 @@
 package se.Grupp2.Quizmaster.models;
 
-
-
 public class Board {
 
+    private Tile[] tiles;
 
-
+    public Board(Tile[] tiles) {
+        this.tiles = tiles;
     }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[] tiles) {
+        this.tiles = tiles;
+    }
+}
 
 
 
