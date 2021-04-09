66ec02fd282a049e6798ea67579c06493bfecd09
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-04-09 08:56:36 +0200

    build structure of playerDTO

--- src/main/java/se/Grupp2/Quizmaster/models/dto/PlayerDTO.java
+++ src/main/java/se/Grupp2/Quizmaster/models/dto/PlayerDTO.java
@@ -1,2 +1,39 @@
-package se.Grupp2.Quizmaster.models.dto;public class PlayerDTO {
+package se.Grupp2.Quizmaster.models.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "players")
+public class PlayerDTO {
+
+    @Id
+    private String userName;
+
+    private String password;
+
+    public PlayerDTO(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public PlayerDTO() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
