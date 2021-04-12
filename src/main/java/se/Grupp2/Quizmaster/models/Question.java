03ee4bec6f1abfe7beb35496e791b08e52d0bcf5
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-04-12 13:44:25 +0200

    Create Question class

--- src/main/java/se/Grupp2/Quizmaster/models/Question.java
+++ src/main/java/se/Grupp2/Quizmaster/models/Question.java
@@ -1,2 +1,21 @@
-package se.Grupp2.Quizmaster.models;public class Question {
+package se.Grupp2.Quizmaster.models;
+
+import java.util.List;
+
+public class Question {
+    private String question;
+    private List<String> answers;
+
+    public Question(String question, List<String> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
 }
