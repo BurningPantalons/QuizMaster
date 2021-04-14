e9fa0a94efd0c17219ca552c9e0f5138635aabaa
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-04-14 15:56:43 +0200

    add testcontroller for api testing, makes /index /game accessible without login for now. Changed token auth time length

--- src/main/java/se/Grupp2/Quizmaster/security/WebSecurityConfig.java
+++ src/main/java/se/Grupp2/Quizmaster/security/WebSecurityConfig.java
@@ -58,6 +58,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/index").permitAll()
+                .antMatchers("/game").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
