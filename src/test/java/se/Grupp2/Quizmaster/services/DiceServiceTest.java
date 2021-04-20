fd757fd12ade06300039e932d3ed5d0739f00fee
sholmqvist <sholmqvist89@gmail.com>
2021-04-20 14:41:17 +0200

    Intial new branch commit

---
+++ src/test/java/se/Grupp2/Quizmaster/services/DiceServiceTest.java
@@ -0,0 +1,11 @@
+package se.Grupp2.Quizmaster.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DiceServiceTest {
+
+    @Test
+    void diceRoll() {
+    }
+}
