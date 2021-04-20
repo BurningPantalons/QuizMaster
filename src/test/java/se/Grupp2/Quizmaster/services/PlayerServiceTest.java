6f8bdd62f7851d81665071882a863642b6bce474
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-04-20 15:17:22 +0200

    add Mockito and init method that runs before all tests

--- src/test/java/se/Grupp2/Quizmaster/services/PlayerServiceTest.java
+++ src/test/java/se/Grupp2/Quizmaster/services/PlayerServiceTest.java
@@ -1,10 +1,27 @@
 package se.Grupp2.Quizmaster.services;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.Grupp2.Quizmaster.dao.PlayerDAO;
+import se.Grupp2.Quizmaster.models.dto.PlayerDTO;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PlayerServiceTest {
+
+    public static PlayerDAO playerDAO;
+    public static PlayerService playerService;
+    public List<PlayerDTO> players;
+
+    @BeforeAll
+    public static void init() {
+        playerDAO = Mockito.mock(PlayerDAO.class);
+        playerService = new PlayerService(playerDAO);
+    }
 
     @Test
     void movePlayer() {
