7d976cf4e842d3aacb7cc1e9bb15ab8814f012ac
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-04-20 17:33:25 +0200

    create test for getAllPlayers method and add toString to Player

--- src/test/java/se/Grupp2/Quizmaster/services/PlayerServiceTest.java
+++ src/test/java/se/Grupp2/Quizmaster/services/PlayerServiceTest.java
@@ -1,13 +1,17 @@
 package se.Grupp2.Quizmaster.services;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import se.Grupp2.Quizmaster.dao.PlayerDAO;
+import se.Grupp2.Quizmaster.models.Player;
 import se.Grupp2.Quizmaster.models.dto.PlayerDTO;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -39,8 +43,38 @@ class PlayerServiceTest {
     void getPlayerById() {
     }
 
-    @Test
-    void getAllPlayers() {
+    @Nested
+    class TestAllPlayers {
+
+        @BeforeEach
+        public void init() {
+            players = new ArrayList<>();
+            players.add(new PlayerDTO(1, "Marcus"));
+            players.add(new PlayerDTO(2, "Rasmus"));
+            players.add(new PlayerDTO(3, "David"));
+            players.add(new PlayerDTO(4, "Stefan"));
+        }
+
+        @Test
+        @DisplayName("Test if you can get all players from db")
+        void getAllPlayers() {
+            List<Player> anticipatedPlayers = new ArrayList<>();
+            anticipatedPlayers.add(new Player(1, "Marcus"));
+            anticipatedPlayers.add(new Player(2, "Rasmus"));
+            anticipatedPlayers.add(new Player(3, "David"));
+            anticipatedPlayers.add(new Player(4, "Stefan"));
+
+
+            Mockito.when(playerDAO.getAllPlayers()).thenReturn(players);
+            List<Player> playersFromDB = playerService.getAllPlayers();
+
+            // Check if the size is correct in the database
+            assertEquals(playersFromDB.size(),anticipatedPlayers.size());
+
+            // Check if the id & names in the database is correct
+            assertEquals(playersFromDB.toString(),anticipatedPlayers.toString());
+
+        }
     }
 
     @Test
