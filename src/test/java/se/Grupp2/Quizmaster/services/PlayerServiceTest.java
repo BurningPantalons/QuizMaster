73398658d154807cda4e49bc6702aecca1bfd6b5
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-04-20 14:35:08 +0200

    add test class, didnt come with last commit

--- src/test/java/se/Grupp2/Quizmaster/services/PlayerServiceTest.java
+++ src/test/java/se/Grupp2/Quizmaster/services/PlayerServiceTest.java
@@ -1,4 +1,32 @@
+package se.Grupp2.Quizmaster.services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerServiceTest {
-  
+
+    @Test
+    void movePlayer() {
+    }
+
+    @Test
+    void addPlayer() {
+    }
+
+    @Test
+    void changeName() {
+    }
+
+    @Test
+    void getPlayerById() {
+    }
+
+    @Test
+    void getAllPlayers() {
+    }
+
+    @Test
+    void deletePlayer() {
+    }
 }
