4094a41d81d3396ff61051fc403de6c79c1a0cae
sholmqvist <sholmqvist89@gmail.com>
2021-04-21 19:42:36 +0200

    Added text structure to build on tests

--- src/test/java/se/Grupp2/Quizmaster/services/DiceServiceTest.java
+++ src/test/java/se/Grupp2/Quizmaster/services/DiceServiceTest.java
@@ -1,11 +1,36 @@
 package se.Grupp2.Quizmaster.services;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("Testing class for DiceService class")
 class DiceServiceTest {
+
+    static DiceService classToBeTested;
+
+    @BeforeAll
+    public static void initAll() {
+        System.out.println("*** Initializing Test with new instance of class to be tested ***");
+        classToBeTested = new DiceService();
+    }
+
+    @BeforeEach
+    public void startOfTest() {
+        System.out.println("> Commencing new individual test");
+    }
+
+    @AfterEach
+    public void endOfTest() {
+        System.out.println("Test completed!");
+    }
 
     @Test
     void diceRoll() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        classToBeTested = null;
+        System.out.println("*** Testing complete. Clering Test ***");
     }
 }
