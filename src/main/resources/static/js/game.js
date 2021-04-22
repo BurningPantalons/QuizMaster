abf40f89ca5c1cc3807ca512034798b19489382d
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-04-22 23:49:00 +0200

    create popup window for the question when clicking on a tile

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,22 +1,26 @@
 $(function(){
     $('body').append('<button onclick="rolledDice()">ROLL DICE</button>');
-    $('body').append('<div class="tileContainer"></div>');
-    tiles.forEach(function (tile) {
-        $('.tileContainer').append(`                  
-        <button onclick="questionWindow()">${tile.position}</button>  
-         `)
-    });
-});
-
-function questionWindow() {
-    $('body').append(`<div class="question-container">
-            <h1 class="question">${question}</h1>
+    $('body').append(`<div class="question-container"><div class="question-content">
+            <h1 style="color: white">${question}</h1>
+            <div class="question-Btn-container">
             <button onclick="checkIfRight('${answer1}')">${answer1}</button>
             <button onclick="checkIfRight('${answer2}')">${answer2}</button>
             <button onclick="checkIfRight('${answer3}')">${answer3}</button>
             <button onclick="checkIfRight('${answer4}')">${answer4}</button>
-            </div>`);
-}
+            </div></div></div>`);
+    $('body').append('<div class="tileContainer"></div>');
+    tiles.forEach(function (tile) {
+        $('.tileContainer').append(`                  
+        <button class="tileBtn">${tile.position}</button>  
+         `)
+    });
+});
+
+$( document ).ready(function() {
+    $(".tileBtn").on("click",function(){
+        $(".question-container").toggle();
+    });
+});
 
 function checkIfRight(answer) {
     if (answer === correctAnswer){
